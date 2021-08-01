8496eb3da65c152f36878dceebea5e02bfbc2765
Douglas-Dorug <douglasx30@gmail.com>
2021-07-31 22:55:45 -0300

    Primeiros desafios de codigo propostos para o Bootcamp everis New Talents - Java

---
+++ src/desafio3/Divisores.java
@@ -0,0 +1,31 @@
+package desafio3;
+
+
+
+/*
+        Ler um número inteiro N e calcular todos os seus divisores.
+
+        Entrada
+
+        O arquivo de entrada contém um valor inteiro.
+
+        Saída
+
+        Escreva todos os divisores positivos de N, um valor por linha.
+*/
+
+
+
+import java.io.IOException;
+import java.util.Scanner;
+
+public class Divisores {
+    public static void main(String[] args) throws IOException {
+        Scanner leitor = new Scanner(System.in);
+        int N = leitor.nextInt();
+        int i;
+        for (i=1; i<= N; i++) {
+            if (N%i == 0) System.out.println(i);
+        }
+    }
+}
